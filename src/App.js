f62eafdc7ee5a9a042d3b8cc66f8cef914061ebe
esinghrahul <e.singh.rahul@outlook.com>
2020-07-01 00:25:15 +0530

    Added styled components and completed the implementation of styled-hero and single-room components

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
     <Navbar />
     <Switch>
     <Route exact path= '/' component= {Home} />
-    <Route path= '/rooms' component= {Rooms} />
+    <Route exact path= '/rooms' component= {Rooms} />
     <Route path= '/rooms/:slug' component= {SingleRoom} />
     <Route component = {Error} />
     </Switch>
     </>
-  );
+  )
 }
 
 export default App;
--- src/pages/single-room.component.jsx
+++ src/pages/single-room.component.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import defaultBackground from '../images/room-1.jpeg'
+import Hero from '../components/hero.component'
+import {RoomContext} from '../context'
+import Banner from '../components/banner.component'
+import StyledHero from '../components/styled-hero.component'
 
 export default class SingleRoom extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            slug: this.props.match.params.slug,
+            defaultBcg: defaultBackground
+        }
+    }
+    static contextType = RoomContext
     render(){
-        return <div>Hello from Single Room Component</div>
+        const {getRoom} = this.context
+        const room= getRoom(this.state.slug)
+        if(!room){
+            return <div className="error">
+                <h3>No such room could be found...</h3>
+                <Link to= '/rooms' className="btn-primary">Back to rooms</Link>
+            </div>
+        }
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+        const [mainImg,...defaultImg] = images
+        return <> 
+                <StyledHero img= {mainImg}>
+                    <Banner title={`${name} room`}>
+                        <Link to= '/rooms' className='btn-primary'>Back to rooms...</Link>
+                    </Banner>
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {defaultImg.map((item, index)=> {
+                            return <img key={index} src={item} alt={name} />
+                        })}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price: ${price}</h6>
+                            <h6>size: {size} Sq.Ft.</h6>
+                            <h6>max capacity: {
+                                capacity > 1 ? `${capacity} people` : `${capacity} person`
+                            }</h6>
+                            <h6>{pets? "pets allowed": "no pets allowed"}</h6>
+                            <h6>{breakfast && "free breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+                <section className="room-extras">
+                    <h6>Extras</h6>
+                        <ul className="extras">
+                            {extras.map((item,index) => {
+                                return <li key={index}>- {item}</li>
+                            })}
+                        </ul>
+                </section>
+        </>
     }
 }
